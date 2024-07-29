91d92eab11686bae20db281ddc8b0fcbaa7017c5
Filipe Siqueira <filipedev040990@gmail.com>
2024-07-29 18:08:00 -0300

    chore: initial config

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,5 +1,11 @@
 import 'module-alias/register'
-import { app } from './app'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 const port = process.env.PORT ?? 3000
 
